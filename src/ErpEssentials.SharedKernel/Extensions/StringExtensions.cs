7fc7f57076e56f69746de1f5db77485eb3477e09
dev-LBAM <lucasbatist.a@hotmail.com>
2025-08-11 11:41:28 -0300

    feat(sharedkernel): Add string extension for name standardization
    -Trims whitespace and applies title casing, preventing data inconsistencies and improving overall data quality before validation

---
+++ src/ErpEssentials.SharedKernel/Extensions/StringExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+
+namespace ErpEssentials.SharedKernel.Extensions;
+
+public static class StringExtensions
+{
+    public static string ToTitleCaseStandard(this string? input)
+    {
+        if (string.IsNullOrWhiteSpace(input))
+        {
+            return string.Empty;
+        }
+
+        var trimmed = input.Trim().ToLower();
+        return CultureInfo.CurrentCulture.TextInfo.ToTitleCase(trimmed);
+    }
+}
