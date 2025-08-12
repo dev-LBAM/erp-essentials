9be7202ccd375b36cd7a441981fb6c7a6e34d10e
dev-LBAM <lucasbatist.a@hotmail.com>
2025-08-12 17:46:44 -0300

    test(domain): Add unit tests for Brand And Category aggregates

---
+++ tests/ErpEssentials.Domain.Tests/Catalog/Categories/CategoryTests.cs
@@ -0,0 +1,67 @@
+﻿using Xunit;
+using ErpEssentials.Domain.Catalog.Categories;
+using ErpEssentials.SharedKernel.ResultPattern;
+using System;
+
+namespace ErpEssentials.Domain.Tests.Catalog.Categories;
+
+public class CategoryTests
+{
+    [Fact]
+    public void Create_Should_ReturnSuccessResult_WhenNameIsValid()
+    {
+        // Arrange
+        string validName = "sport shoes";
+
+        // Act
+        Result<Category> result = Category.Create(validName);
+
+        // Assert
+        Assert.True(result.IsSuccess);
+        Assert.NotNull(result.Value);
+        Assert.Equal("Sport Shoes", result.Value.Name);
+    }
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    public void Create_Should_ReturnFailureResult_WhenNameIsInvalid(string? invalidName)
+    {
+        // Act
+        Result<Category> result = Category.Create(invalidName!);
+
+        // Assert
+        Assert.True(result.IsFailure);
+        Assert.Equal(CategoryErrors.EmptyName.Code, result.Error.Code);
+    }
+
+    [Fact]
+    public void UpdateName_Should_Succeed_And_StandardizeName_WhenNameIsValid()
+    {
+        // Arrange
+        Category category = Category.Create("eletronics").Value;
+        string newName = "  smart eletronics  ";
+
+        // Act
+        Result result = category.UpdateName(newName);
+
+        // Assert
+        Assert.True(result.IsSuccess);
+        Assert.Equal("Smart Eletronics", category.Name);
+    }
+
+    [Fact]
+    public void UpdateName_Should_Fail_WhenNewNameIsInvalid()
+    {
+        // Arrange
+        Category category = Category.Create("furniture").Value;
+        string invalidName = "   ";
+
+        // Act
+        Result result = category.UpdateName(invalidName);
+
+        // Assert
+        Assert.True(result.IsFailure);
+        Assert.Equal(CategoryErrors.EmptyName.Code, result.Error.Code);
+    }
+}
